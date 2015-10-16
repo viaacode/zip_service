85d403b7a27e3321aa67cd3c7f1ede9d22eed6b0
Jonas Liekens <jonas.liekens@c4j.be>
2015-10-16 11:13:02 +0200

    Expanded worker to accept config file

--- README.md
+++ README.md
@@ -15,13 +15,33 @@ Simply check out this repository and run:
 You can run the worker by executing the following command:
 
 ```
-    zipper [-h] [-p BROKER_PORT] [-q RESULT_QUEUE] [-t TOPIC_TYPE] broker_ip incoming_queue result_exchange result_routing username password
+    zipper [-h] [--broker_ip BROKER_IP]
+                  [--incoming_queue INCOMING_QUEUE]
+                  [--result_exchange RESULT_EXCHANGE]
+                  [--result_routing RESULT_ROUTING] [--username USERNAME]
+                  [--password PASSWORD] [--broker_port BROKER_PORT]
+                  [--result_queue RESULT_QUEUE] [--topic_type TOPIC_TYPE]
 ```
 
 Information about every parameter can be consulted with:
 
 ```
     zipper -h
+```
+
+The worker can be configured with a zipper.conf file that has be located in /etc/viaa-workers. If it doesn't exists the command line arguments will be used. You can use command line arguments to overrule property values.
+
+```
+[DEFAULT]
+BROKER_IP=192.168.56.101                #The ip of the RabbitMQ Broker
+BROKER_PORT=5672                        #The port of the RabbitMQ Broker, comment out if not needed. This will default to 5672
+INCOMING_QUEUE=incoming_queue           #The name of the queue the worker will listen to
+RESULT_EXCHANGE=result_exchange         #The name of result exchange where the worker will publish its result messages to
+RESULT_ROUTING=result_routing           #The name of the routing key to be used to publish messages
+RESULT_QUEUE=result_queue               #The name of the result queue if this doesn't exist yet. Comment out if not needed
+TOPIC_TYPE=direct                       #The name of the topic type for the result queue. Comment out if not needed
+USERNAME=guest                          #The username to access the RabbitMQ broker
+PASSWORD=guest                          #The passwrod to access the RabbitMQ broker
 ```
 
 # Documentation
--- zipper/command_line.py
+++ zipper/command_line.py
@@ -1,22 +1,35 @@
 import argparse
 from .worker import Consumer
 from .arguments import Arguments
+from configparser import ConfigParser
+from os.path import exists
+from sys import exit
 
 
 def main():
     arguments = Arguments()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('broker_ip', help='the ip adress/hostname of the rabbit cluster', type=str)
-    parser.add_argument('incoming_queue', help='the incoming queue to bind to for incoming messages', type=str)
-    parser.add_argument('result_exchange', help='the exchange to send the result to', type=str)
-    parser.add_argument('result_routing', help='the routing key to use when posting the result', type=str)
-    parser.add_argument('username', help='your rabbitMQ username', type=str)
-    parser.add_argument('password', help='your rabbitMQ password', type=str)
-    parser.add_argument('-p', '--broker_port', help='the broker port (defaults to: 5672)', type=int, default=5672)
-    parser.add_argument('-q', '--result_queue', help='queue to be declared if none exists', type=str, default=None)
-    parser.add_argument('-t', '--topic_type', help='the type of the exchange should it be created', type=str, default=None)
+    parser.add_argument('--broker_ip', help='the ip adress/hostname of the rabbit cluster', type=str)
+    parser.add_argument('--incoming_queue', help='the incoming queue to bind to for incoming messages', type=str)
+    parser.add_argument('--result_exchange', help='the exchange to send the result to', type=str)
+    parser.add_argument('--result_routing', help='the routing key to use when posting the result', type=str)
+    parser.add_argument('--username', help='your rabbitMQ username', type=str)
+    parser.add_argument('--password', help='your rabbitMQ password', type=str)
+    parser.add_argument('--broker_port', help='the broker port (defaults to: 5672)', type=int)
+    parser.add_argument('--result_queue', help='queue to be declared if none exists', type=str)
+    parser.add_argument('--topic_type', help='the type of the exchange should it be created', type=str)
     parser.parse_args(namespace=arguments)
+
+    conf_path = '/etc/viaa-workers/zipper.conf'
+
+    print("Initializing...")
+    if exists(conf_path):
+        parse_config(arguments, conf_path)
+    else:
+        print("WARNING: No config file found, using command line arguments...")
+
+    check_arguments(arguments)
 
     try:
         print("Starting...use CTRL-C to exit.")
@@ -24,6 +37,73 @@ def main():
         consumer.consume()
     except KeyboardInterrupt:
         print("Exited.")
+
+
+def parse_config(arguments, conf_path):
+    parser = ConfigParser()
+    parser.read(conf_path)
+    config = parser['DEFAULT']
+
+    if arguments.broker_ip is None:
+        arguments.broker_ip = config['BROKER_IP']
+
+    if arguments.incoming_queue is None:
+        arguments.incoming_queue = config['INCOMING_QUEUE']
+
+    if arguments.result_exchange is None:
+        arguments.result_exchange = config['RESULT_EXCHANGE']
+
+    if arguments.result_routing is None:
+        arguments.result_routing = config['RESULT_ROUTING']
+
+    if arguments.username is None:
+        arguments.username = config['USERNAME']
+
+    if arguments.password is None:
+        arguments.password = config['PASSWORD']
+
+    if arguments.broker_port is None:
+        try:
+            arguments.broker_port = config['BROKER_PORT']
+        except KeyError:
+            arguments.broker_port = 5672
+
+    if arguments.result_queue is None:
+        try:
+            arguments.result_queue = config['RESULT_QUEUE']
+        except KeyError:
+            print("WARNING: No result queue specified")
+
+    if arguments.topic_type is None:
+        try:
+            arguments.topic_type = config['TOPIC_TYPE']
+        except KeyError:
+            print("WARNING: No topic type specified")
+
+
+def check_arguments(arguments):
+    if arguments.broker_ip is None:
+        close("ERROR: No broker ip specified")
+
+    if arguments.incoming_queue is None:
+        close("ERROR: No incoming queue specified")
+
+    if arguments.result_exchange is None:
+        close("ERROR: No result exchange specified")
+
+    if arguments.result_routing is None:
+        close("ERROR: No result routing specified")
+
+    if arguments.username is None:
+        close("ERROR: No username specified")
+
+    if arguments.password is None:
+        close("ERROR: No password specified")
+
+
+def close(message):
+    print(message)
+    exit(1)
 
 if __name__ == "__main__":
     main()
