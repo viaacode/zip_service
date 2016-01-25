6482289fd3ba4ef3533a35265e806282c4704b7b
Jonas Liekens <jonas.liekens@c4j.be>
2016-01-25 16:51:45 +0100

    Added Message Validation and more robust error hadling

--- zipper/worker.py
+++ zipper/worker.py
@@ -7,6 +7,26 @@ from .rabbit_publisher import send_message
 from . import zipper
 
 
+def validate_message(params):
+    mandatory_keys = [
+        'correlation_id',
+        'source_server',
+        'source_path',
+        'destination_server',
+        'destination_path',
+        'destination_file'
+    ]
+
+    message_valid = True
+
+    for key in mandatory_keys:
+        if key not in params:
+            message_valid = False
+            logging.error('{} is missing from received message'.format(key))
+
+    return message_valid
+
+
 class Consumer:
     def __init__(self, arguments):
         self.host = arguments.broker_ip
@@ -20,54 +40,58 @@ class Consumer:
         self.topic_type = arguments.topic_type
 
     def consume(self):
-        try:
-            connection = pika.BlockingConnection(pika.ConnectionParameters(
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
                 host=self.host,
                 port=self.port,
                 credentials=PlainCredentials(self.username, self.password)
-            ))
+        ))
 
-            channel = connection.channel()
-            channel.queue_declare(queue=self.queue, durable=True)
-            channel.basic_consume(self.callback, self.queue)
-            channel.start_consuming()
-        except Exception as e:
-            logging.error(str(e))
+        channel = connection.channel()
+        channel.queue_declare(queue=self.queue, durable=True)
+        channel.basic_consume(self.callback, self.queue)
+        channel.start_consuming()
 
     def callback(self, ch, method, properties, body):
-        params = loads(body.decode("utf-8"))
-        status = 'OK'
-        details = 'Zipfile Created.'
-
         try:
-            zipper.zip_dir(params['source_path'], params['destination_path'], params['destination_file'])
+            params = loads(body.decode("utf-8"))
+            status = 'OK'
+            details = 'Zipfile Created.'
+
+            if validate_message(params):
+                try:
+                    zipper.zip_dir(params['source_path'], params['destination_path'], params['destination_file'])
+                except Exception as e:
+                    status = 'NOK'
+                    details = str(e)
+
+                message = {
+                    "correlation_id": params["correlation_id"],
+                    "status": status,
+                    "description": details,
+                    "destination_server": params["destination_server"],
+                    "destination_path": params["destination_path"],
+                    "destination_file": params["destination_file"],
+                    "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                }
+
+                json_message = dumps(message)
+                logging.info(json_message)
+
+                send_message(
+                        self.host,
+                        self.port,
+                        self.username,
+                        self.password,
+                        self.result_exchange,
+                        self.result_routing,
+                        self.result_queue,
+                        self.topic_type,
+                        json_message
+                )
+            else:
+                logging.error('Message invalid: {}'.format(params))
+
+            ch.basic_ack(delivery_tag=method.delivery_tag)
         except Exception as e:
-            status = 'NOK'
-            details = str(e)
-
-        message = {
-            "correlation_id": params["correlation_id"],
-            "status": status,
-            "description": details,
-            "destination_server": params["destination_server"],
-            "destination_path": params["destination_path"],
-            "destination_file": params["destination_file"],
-            "timestamp": datetime.datetime.now()
-        }
-
-        json_message = dumps(message)
-        logging.info(json_message)
-
-        send_message(
-            self.host,
-            self.port,
-            self.username,
-            self.password,
-            self.result_exchange,
-            self.result_routing,
-            self.result_queue,
-            self.topic_type,
-            json_message
-        )
-
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+            logging.error(str(e))
